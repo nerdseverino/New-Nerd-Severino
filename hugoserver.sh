9ae9000437f23651f949aae981dbafc5e597a8de
Fabrício Machado <machado.computer@gmail.com>
2020-06-14 00:56:39 -0300

    Commit 13062020 11

--- hugoserver.sh
+++ hugoserver.sh
@@ -11,5 +11,9 @@
 
 docker stop hugo-server
 docker rm   hugo-server
-#docker run -tip 1313:1313 -v $(pwd):/home/circleci/project:cached -e HUGO_THEME=devopsdays-theme -e HUGO_BASEURL="http://localhost:1313" --name hugo-server --entrypoint "" cibuilds/hugo:0.67.0 hugo server --watch --bind ""
-docker run -tip 1313:1313 -v $(pwd):/home/circleci/project:cached -e HUGO_BASEURL="http://localhost:1313" --name hugo-server --entrypoint "" cibuilds/hugo:0.67.0 hugo server --watch --bind ""
+
+#docker run -tip 1313:1313 -v $(pwd):/home/circleci/project:cached -e HUGO_BASEURL="http://localhost:1313" --name hugo-server --entrypoint "" cibuilds/hugo:0.67.0 hugo server --watch --bind ""
+
+#docker run -tip 1313:1313 -v $(pwd):/home/circleci/project:cached -e HUGO_BASEURL="http://localhost:1313" --name hugo-server --entrypoint "" hypriot/rpi-hugo:0.67.0 hugo server --watch --bind ""
+
+docker run -d -p 1313:1313 -v $(pwd):/www hypriot/rpi-hugo server --bind "" -w -D
