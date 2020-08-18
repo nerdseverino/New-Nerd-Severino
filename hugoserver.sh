7116845630a5a928f78430c39d03959fcbf9e096
Fabrício Machado <machado.computer@gmail.com>
2020-08-18 16:44:42 -0300

    Update hugoserver.sh
    incluido variável para pegar o IP da máquina e a linha pra subir o container

--- hugoserver.sh
+++ hugoserver.sh
@@ -7,6 +7,7 @@
 # This requires Docker 17.06.0-ce or above.
 #
 # NOTE: The docker image provided by jojomi is not managed or audited by devopsdays
+ip=`ip addr show | grep global | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' | sed -n '1p'`
 
 
 docker stop hugo-server
@@ -16,4 +17,6 @@ docker rm   hugo-server
 
 #docker run -tip 1313:1313 -v $(pwd):/home/circleci/project:cached -e HUGO_BASEURL="http://localhost:1313" --name hugo-server --entrypoint "" hypriot/rpi-hugo:0.67.0 hugo server --watch --bind ""
 
-docker run -d -p 1313:1313 -v $(pwd):/www hypriot/rpi-hugo server --bind "" -w -D
+#docker run -d -p 1313:1313 -v $(pwd):/www hypriot/rpi-hugo server --bind "" -w -D
+
+docker run -d -p 1313:1313 --name hugo-server -v $(pwd):/www hypriot/rpi-hugo server -b http://$ip/ --bind=0.0.0.0 -w -D --theme=hyde-x
