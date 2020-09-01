53dcf5d7a4e1c8366cdb33d7716f36c85fc3e807
Fabricio Machado <machado.computer@gmail.com>
2020-09-01 09:37:24 -0300

    acertando o bash hugo server

--- hugoserver.sh
+++ hugoserver.sh
@@ -11,11 +11,11 @@ ip=`ip addr show | grep global | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\
 
 docker stop hugo-server
 docker rm   hugo-server
-docker stop portainer
-docker rm   portainer
+#docker stop portainer
+#docker rm   portainer
 
 docker run -d -p 1313:1313 --name hugo-server -v $(pwd):/www hypriot/rpi-hugo server -b http://$ip/ --bind=0.0.0.0 -w -D --theme=hyde-x
-docker run -d -p 9000:9000 --name portainer  -v /var/run/docker.sock:/var/run/docker.sock hypriot/rpi-portainer
+#docker run -d -p 9000:9000 --name portainer  -v /var/run/docker.sock:/var/run/docker.sock hypriot/rpi-portainer
 
 #old
 #docker run -tip 1313:1313 -v $(pwd):/home/circleci/project:cached -e HUGO_BASEURL="http://localhost:1313" --name hugo-server --entrypoint "" cibuilds/hugo:0.67.0 hugo server --watch --bind ""
